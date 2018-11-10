d1713fd0532f221e22f7cc2b2619d0016d090d27
Madhu Sudhan <madhu.subedi@monotype.com>
2018-11-10 16:20:52 +0545

    Project delete feature is also completed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,7 +36,14 @@ class ProjectsController < ApplicationController
       flash.now[:alert] = "Project has not been updated."
       return "edit"
     end
+  end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
   end
 
   private
